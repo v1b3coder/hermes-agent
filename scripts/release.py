7a27735b4adffc0140fcbfc94c0379ab0ab11394
Teknium <127238744+teknium1@users.noreply.github.com>
2026-05-09 13:19:56 -0700

    chore: add mbac to AUTHOR_MAP

--- scripts/release.py
+++ scripts/release.py
@@ -64,6 +64,7 @@ AUTHOR_MAP = {
     "ytchen0719@gmail.com": "liquidchen",
     "am@studio1.tailb672fe.ts.net": "subtract0",
     "axmaiqiu@gmail.com": "qWaitCrypto",
+    "308068+mbac@users.noreply.github.com": "mbac",
     "wesleysimplicio@live.com": "wesleysimplicio",
     "matthew.dean.cater@gmail.com": "SiliconID",
     "xieniu@proton.me": "xieNniu",
